648f03461b7c92d080869055b27a8d6e91d0b6e6
Jameson Nash <vtjnash@gmail.com>
2017-06-21 10:14:18 -0700

    isolate each test run into a separate process

--- src/CoverageBase.jl
+++ src/CoverageBase.jl
@@ -22,10 +22,8 @@ end
 
 module BaseTestRunner
 import ..julia_top
-let topdir = julia_top(),
-    testdir = joinpath(topdir, "test")
-include(joinpath(testdir, "choosetests.jl"))
-include(joinpath(testdir, "testdefs.jl"))
+let testdir = joinpath(julia_top(), "test")
+    include(joinpath(testdir, "choosetests.jl"))
 end
 end
 
@@ -40,14 +38,41 @@ function testnames()
     names
 end
 
+function julia_cmd()
+    julia = Base.julia_cmd()
+    inline = Base.JLOptions().can_inline == 0 ? "no" : "yes"
+    cc = ("none", "user", "all")[Base.JLOptions().code_coverage + 1]
+    return `$julia --precompiled=no --inline=$inline --code-coverage=$cc`
+end
+
 function runtests(names)
     topdir = julia_top()
     testdir = joinpath(topdir, "test")
+    testrunner = joinpath(@__DIR__, "testrunner.jl")
+    julia = julia_cmd()
+    script = """
+        include("testdefs.jl")
+        @time testresult = runtests(ARGS[1])
+        # TODO: exit(testresult.anynonpass ? 1 : 0)
+        """
+    fail = false
     cd(testdir) do
         for tst in names
-            @time BaseTestRunner.runtests(tst)
+            print_with_color(:bold, "RUNTEST: $tst\n")
+            cmd = Cmd(`$julia -e $script -- $tst`, dir = testdir)
+            try
+                success(spawn(cmd, DevNull, STDOUT, STDERR))
+            catch err
+                bt = catch_backtrace()
+                println(STDERR)
+                println(STDERR, "-"^40)
+                Base.display_error(STDERR, err, bt)
+                println(STDERR, "-"^40)
+                fail = true
+            end
         end
     end
+    !fail
 end
 
 end # module
